225cc5ca8843f73343940a52af27f111ec5d0315
David Hürlimann <david@cyt.ch>
2014-02-23 20:34:06 +0100

    Added a new Locomotive CMS.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThekleineCh::Application.config.secret_token = '1b7d5f7f067591eeddea7e75ec0fa5b413bed011ae9d981c4f9a57ad67cf430989b8e9e19f9cac1ab8749af0f30950fa88c1114db7bc8bad242186ddf993ef94'
